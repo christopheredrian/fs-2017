701011557b7ed450982cdfe06a096e8b7dde7cdd
Christopher Espiritu <christopheredrian@gmail.com>
2017-12-03 22:56:14 +0800

    Added initial chart of accounts

---
+++ resources/views/account/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="box">
+        <div class="box-header">
+            <h1>Create new Account</h1>
+        </div>
+        <div class="box-body">
+
+            <form class="form-horizontal"  action="{{ url('/accounts') }}" method="post">
+                {{ csrf_field() }}
+                <div class="box-body">
+                    <div class="form-group">
+                        <label for="name" class="col-sm-2 control-label">Account Name</label>
+                        <div class="col-sm-10">
+                            <input name="name" type="text" class="form-control" id="name" placeholder="Account Name..." required>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+                <div class="box-footer">
+                    <button type="submit" class="btn btn-default">Cancel</button>
+                    <button type="submit" class="btn btn-success pull-right">Create</button>
+                </div>
+                <!-- /.box-footer -->
+            </form>
+        </div>
+    </div>
+
+@endsection
---
+++ resources/views/account/index.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="box">
+        <div class="box-header">
+            <h1>Chart of Accounts</h1>
+        </div>
+        <div class="box-body">
+            <a class="btn btn-success" href="{{ url('/accounts/create') }}">Create</a>
+            <table class="table table-striped table-condensed table-bordered">
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Account Name</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($accounts as $account)
+                    <tr>
+                        <td>{{ $account->id }}</td>
+                        <td>{{ $account->name }}</td>
+                        <td><a href="{{ url("/accounts/$account->id/edit") }}" class="btn btn-warning btn-xs">Edit</a></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+@endsection
