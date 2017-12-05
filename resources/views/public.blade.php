54151519e6a3042f9619fd4bb56cea1769f30afb
Christopher Espiritu <christopheredrian@gmail.com>
2017-12-05 22:44:40 +0800

    Added Contents to about page

---
+++ resources/views/public.blade.php
@@ -0,0 +1,73 @@
+@extends('layouts.app')
+
+@section('styles')
+
+@endsection
+@section('content')
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="box">
+                <div class="box-header page-header">
+                    <h1>Financial Systems Final Project</h1>
+                    <p class="text-justify">This capstone project is a manifestation of our subject financial systems.
+                        The is the front-end
+                        as well as (in-addition) a back-end implementation of the system and is only one of the
+                        deliverables required to accomplish the project</p>
+                    <p>The project is submitted to <strong><a href="https://www.facebook.com/jonutz">Mr. Jonathan A.
+                                Ramirez</a></strong></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="box">
+                <div class="box-header page-header">
+                    <h2>Documentation <i class="fa fa-file-text"></i></h2>
+                    <p>The documentation can be found in the links provided below</p>
+                    <ul>
+                        <li>
+                            <a href="https://docs.google.com/document/d/1BUyTB0AFcuy7csvf6ZYzpyGpU5sWaQqvnJEFj8u6DB8/edit?usp=sharing">Google
+                                Docs</a>
+                            (<a href="https://docs.google.com/document/d/1BUyTB0AFcuy7csvf6ZYzpyGpU5sWaQqvnJEFj8u6DB8/export?format=pdf">PDF <i class="fa fa-file-pdf-o"></i></a>)
+                        </li>
+                        <li>
+                            <a href="https://docs.google.com/spreadsheets/d/1dQhz8byiQqePl-HzBe-CYa1FhpjumnIcp7Cubj2CuDw/edit?usp=sharing">Google
+                                Sheets</a>
+                            (<a href="https://docs.google.com/spreadsheets/d/1dQhz8byiQqePl-HzBe-CYa1FhpjumnIcp7Cubj2CuDw/export?format=xlsx">XLS <i class="fa fa-file-excel-o"></i></a>)
+
+                        </li>
+                    </ul>
+                    <p>As well as the code repository and the link to <strong>this</strong> web application</p>
+                    <ul>
+                        <li><a href="https://github.com/christopheredrian/fs-2017">Github</a></li>
+                        <li><a href="http://fs-2017.herokuapp.com">Heroku App (deployed)</a></li>
+                    </ul>
+                </div>
+                <div class="box-body">
+
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="box">
+                <div class="box-header page-header">
+                    <h2> Members <i class="fa fa-users"></i></h2>
+                    <p>The group is composed of seven members</p>
+                    <ul>
+                        <li>Agusdan, Arnel C.</li>
+                        <li>Almayda, Audry Jane L.</li>
+                        <li>Espiritu, Christopher Edrian L.</li>
+                        <li>Ignacio, Hazel B.</li>
+                        <li>Laban, Chad</li>
+                        <li>Salazar, Von</li>
+                        <li>Willy, Kim</li>
+                    </ul>
+                </div>
+                <div class="box-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
